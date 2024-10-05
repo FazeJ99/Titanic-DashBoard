f6748f41bc2a80c2bff987b21fb00977da64dac4
Justin Ngugi <justsane12@gmail.com>
2024-10-05 09:45:24 +0300

    Changing file path

--- titanic.py
+++ titanic.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import plotly.express as px
 
 @st.cache_data  # 👈 Add the caching decorator
 def load_data(url):
     df = pd.read_csv(url)
     return df
 
-df = load_data("dataset\Titanic Data.csv")
+df = load_data("https://raw.githubusercontent.com/FazeJ99/titanic-dash/refs/heads/main/dataset/Titanic%20Data.csv")
 st.dataframe(df)
+df.head()
 
 st.button("Rerun")
+
+st.bar_chart(data=df, x = 'Sex' , y= 'Pclass')
+
+fig = px.pie(df , values= 'Survived', names='Sex', title="Survival according to Gender")
+
+st.plotly_chart(fig)
