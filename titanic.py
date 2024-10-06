1be140a12d498c6e4ba719673217ad6236d4bed1
Justin Ngugi <justsane12@gmail.com>
2024-10-06 10:15:19 +0300

    Adding visuals and analysis

--- titanic.py
+++ titanic.py
@@ -22,5 +22,24 @@ fig = px.pie(df , values= 'Survived', names='Sex', title="Survival according to 
 
 st.plotly_chart(fig)
 
+# Scatter plot of Age against Fare
 st.write("Scatter Chart of Age against Fare")
 st.scatter_chart(data=df, x = 'Age' , y = 'Fare')
+
+# Histogram of Age of Survivors
+st.write("Histogram of Age of Survivors")
+fig = px.histogram(df , x='Age' , color='Survived')
+st.plotly_chart(fig)
+
+
+st.write("Box plot to compare age distribution across different classes")
+# Box Plot: To compare age distribution across different classes
+fig = px.box(df , y  = 'Age' , color = 'Pclass')
+st.plotly_chart(fig)
+
+
+st.write("Survival Rate of Passengers based on the Ports embarked(Southampton,Cherbourg,Queenstown)")
+# Bar Chart: To analyze the survival rate of passengers based on the port they embarked from
+fig = px.bar(df , x = 'Embarked' , y = 'Survived')
+st.plotly_chart(fig)
+
